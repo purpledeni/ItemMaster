54502dd5946187452a9a1e4b725e2126bde19ce1
Denis <96981600+purpledeni@users.noreply.github.com>
2024-12-22 01:06:14 +0200

    clean ups

--- avatar/ItemMaster/updater.lua
+++ avatar/ItemMaster/updater.lua
@@ -7,10 +7,11 @@ local Promise = require("ItemMaster.Promise")
 local debugmode = true
 
 local function message(str,color,actionbar)
+    if not color then color = 'white' end
     if actionbar then
         host:setActionbar('["[IM] ",{"text":"' .. str .. '","color":"' .. color .. '"}]')
     else
-        printJson('["[ItemMaster] ",{"text":"' .. str .. '","color":"' .. color .. '"}]')
+        printJson('["\n[ItemMaster] ",{"text":"' .. str .. '","color":"' .. color .. '"}]')
     end
 end
 
@@ -19,11 +20,11 @@ local function checkNetworking()
         if net:isLinkAllowed('https://api.github.com') and net:isLinkAllowed('https://raw.githubusercontent.com') then
             return true
         else
-            message('Host \\"api.github.com\\" or \\"raw.githubusercontent.com\\" not whitelisted.', "red")
+            message('Host \\"api.github.com\\" or \\"raw.githubusercontent.com\\" are not whitelisted.', "red")
             return false
         end
     else
-        message('Networking API disabled.', "red")
+        message('Networking API is disabled.', "red")
         return false
     end
 end
@@ -62,51 +63,48 @@ local function recursiveDelete(dir,thisone)
     if thisone then file:delete(dir) end
 end
 
-local function checkVersion()
-    if checkNetworking() then
-        --fetch('https://raw.githubusercontent.com/purpledeni/ItemMaster/' .. branch .. '/assets/VERSION', 'raw')
-        Promise.awaitGet('https://raw.githubusercontent.com/purpledeni/ItemMaster/' .. branch .. '/assets/VERSION')
-        :thenString(function(str)
-            log(tonumber(readByteArray('ItemMaster/assets/VERSION')))
-        end)
-    end
-end
-
 local function fetchAssets()
     if checkNetworking() then
         recursiveDelete('ItemMaster/assets',false)
-        --fetch('https://api.github.com/repos/purpledeni/ItemMaster/git/trees/main?recursive=1', 'tree')
         Promise.awaitGet('https://api.github.com/repos/purpledeni/ItemMaster/git/trees/' .. branch .. '?recursive=1')
         :thenJson(function(json)
-            table.sort(json.tree, function(a,b) 
-                if a.type == 'tree' and b.type == 'blob' then
-                    return true -- tree comes before blob
-                else
-                    return false
-                end
-             end)
+            table.sort(json.tree, function(a,b)
+                return a.type == 'tree' and b.type == 'blob' -- tree comes before blob
+            end)
             for i, v in pairs(json.tree) do
                 if string.find(v.path, "^assets/") then
                     if v.type == 'tree' then
                         if file:mkdirs('ItemMaster/' .. v.path) then
-                            if debugmode then message('Created folder : ItemMaster/' .. v.path, 'white') end
+                            if debugmode then message('Created folder : ItemMaster/' .. v.path) end
                         end
                     elseif v.type == 'blob' then
                         --file:writeString()
                         Promise.awaitGet('https://raw.githubusercontent.com/purpledeni/ItemMaster/' .. branch .. '/' .. v.path)
                         :thenByteArray(function(str)
-                            log(str)
+                            --log(str)
                             writeByteArray('ItemMaster/' .. v.path, str)
-                            if debugmode then message('Written to : ItemMaster/' .. v.path, 'white') end
+                            if debugmode then message('Written to : ItemMaster/' .. v.path) end
                         end)
                     end
-                    log(v.path)
+                    --log(v.path)
                 end
             end
         end)
     end
 end
 
+local function checkVersion()
+    if checkNetworking() then
+        Promise.awaitGet('https://raw.githubusercontent.com/purpledeni/ItemMaster/' .. branch .. '/assets/VERSION')
+        :thenString(function(str)
+            local localversion = tonumber(readByteArray('ItemMaster/assets/VERSION'))
+            if localversion < tonumber(str) then
+                if debugmode then message('Currently installed version (' .. localversion .. ') is outdated (' .. str .. '), updating...', "blue") end
+                fetchAssets()
+            end
+        end)
+    end
+end
 
 if not file:mkdirs('ItemMaster/assets') and file:exists('ItemMaster/assets/VERSION') then
     checkVersion()
